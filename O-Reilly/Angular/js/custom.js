65070655fcf8958c84c059a60665ca824f6740f3
Richard Littauer <richard.littauer@gmail.com>
2017-06-23 16:33:32 -0400

    Remove old dashboard

--- O-Reilly/Angular/js/custom.js
+++
@@ -1,8 +0,0 @@
-$("th").on('click',function() {
-	var caret = $(this).find('.glyphicon');
-	if(caret.hasClass('glyphicon-chevron-down')) {
-		caret.removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-	} else {
-		caret.removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-	}
-});
--- O-Reilly/Node/node_modules/logfmt/examples/simple_cmd.js
+++
@@ -1,8 +0,0 @@
-var logfmt = require('../logfmt');
-var through = require('through');
-
-process.stdin
-  .pipe(logfmt.streamParser())
-  .pipe(through(function(object){
-    console.log(object);
-  }))
